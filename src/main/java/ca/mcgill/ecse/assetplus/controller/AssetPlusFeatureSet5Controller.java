a0a7815383b67afd53f10ab9b8b14ecf6869b462
Dmytro <dmytro.martyniuk@mail.mcgill.ca>
2023-10-12 22:26:09 -0400

    Initial features added

--- src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet5Controller.java
+++ src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet5Controller.java
@@ -1,15 +1,96 @@
 package ca.mcgill.ecse.assetplus.controller;
 
+import ca.mcgill.ecse.assetplus.model.MaintenanceTicket;
+import ca.mcgill.ecse.assetplus.model.TicketImage;
+
 public class AssetPlusFeatureSet5Controller {
 
-  public static String addImageToMaintenanceTicket(String imageURL, int ticketID) {
-    // Remove this exception when you implement this method
-    throw new UnsupportedOperationException("Not Implemented!");
-  }
+    // Method to validate the inputs
+    public static Boolean isValid(String imageURL, int ticketID) throws IllegalArgumentException {
 
-  public static void deleteImageFromMaintenanceTicket(String imageURL, int ticketID) {
-    // Remove this exception when you implement this method
-    throw new UnsupportedOperationException("Not Implemented!");
-  }
+        var error = "";
+        if (imageURL == null || imageURL == "") {
+            error = "Image Url cannot be empty.";
+            throw new IllegalArgumentException(error);
+        }
 
+        if (ticketID < 0) {
+            error = "Invalid ticket ID";
+            throw new IllegalArgumentException(error);
+        }
+
+        if (!imageURL.startsWith("http://") && !imageURL.startsWith("https://")) {
+            error = "Invalid image URL";
+            throw new IllegalArgumentException(error);
+        }
+
+        return true;
+
+    }
+
+    public static String addImageToMaintenanceTicket(String imageURL, int ticketID) {
+
+        var error = "";
+
+        if (isValid(imageURL, ticketID)) {
+            try {
+                MaintenanceTicket ticket = MaintenanceTicket.getWithId(ticketID);
+
+                // Check if the imageURL is already in images in the ticket.
+                for (TicketImage image : ticket.getTicketImages()) {
+                    if (image.getImageURL().equals(imageURL)) {
+                        error = "Cannot add the same image twice";
+                        throw new IllegalArgumentException(error);
+                    }
+                }
+
+                // Do I have to do that ?
+                if (error.isEmpty()) {
+                    // Create a new ticket image object with an existing ticket
+                    TicketImage ticketImage = new TicketImage(imageURL, ticket);
+
+                    // Add the ticket image to the ticket
+                    ticket.addTicketImage(ticketImage);
+                }
+            } catch (RuntimeException e) {
+                error = e.getMessage();
+            }
+        }
+
+        return error;
+    }
+
+    public static void deleteImageFromMaintenanceTicket(String imageURL, int ticketID) {
+
+        var error = "";
+
+        if (isValid(imageURL, ticketID)) {
+            try {
+                MaintenanceTicket ticket = MaintenanceTicket.getWithId(ticketID);
+
+                // Searches for the correct image to be removed from the ticket. BY cheking the
+                // urls of all iamges in the ticket
+                boolean imageFound = false;
+                for (TicketImage image : ticket.getTicketImages()) {
+                    if (image.getImageURL().equals(imageURL)) {
+                        // remove the image from the maintenace ticket
+                        ticket.removeTicketImage(image);
+                        // Deletes the image
+                        image.delete();
+                        // Should the program exit ?
+                        imageFound = true;
+                        break; // Should I Exit the loop since the image is found ?
+                    }
+                }
+
+                if (!imageFound) {
+                    error = "Image provided is not in the ticket";
+                    throw new IllegalArgumentException(error);
+                }
+
+            } catch (RuntimeException e) {
+                error = e.getMessage();
+            }
+        }
+    }
 }
