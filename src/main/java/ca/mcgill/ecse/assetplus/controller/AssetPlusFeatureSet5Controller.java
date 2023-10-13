5fe9431282b31a8f9786f8d7d2ad9794ee5a7f4c
Dmytro <dmytro.martyniuk@mail.mcgill.ca>
2023-10-13 19:03:53 -0400

    feature 5 update

--- src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet5Controller.java
+++ src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet5Controller.java
@@ -7,25 +7,25 @@ public class AssetPlusFeatureSet5Controller {
 
     public static String addImageToMaintenanceTicket(String imageURL, int ticketID) {
 
-        var error = "";
+        StringBuilder error = new StringBuilder();
 
         if (imageURL == null || imageURL == "") {
-            error = "Image URL cannot be empty ";
-            return error;
+            error.append("Image URL cannot be empty\n'");
+        } else if (!imageURL.startsWith("http://") && !imageURL.startsWith("https://")) {
+            error.append("Image URL must start with http:// or https://\n");
         }
         if (ticketID < 0) {
-            error = "Ticket does not exist";
-            return error;
-        }
-        if (!imageURL.startsWith("http://") && !imageURL.startsWith("https://")) {
-            error = "Image URL must start with http:// or https://";
-            return error;
+            error.append("Ticket does not exist\n'");
         }
         if( !MaintenanceTicket.hasWithId(ticketID) ){
-            error ="Ticket does not exist ";
-            return error;
+            error.append("Ticket does not exist\n ");
         }
 
+        if (!error.isEmpty()){
+            String result = error.toString();
+            return result;
+        }
+        
         try{
             //Initialize a new ticket object. We know there is one since no errors.
             MaintenanceTicket ticket = MaintenanceTicket.getWithId(ticketID);
@@ -33,8 +33,9 @@ public class AssetPlusFeatureSet5Controller {
             // Check if the imageURL is already in images in the ticket.
             for (TicketImage image : ticket.getTicketImages()) {
                 if (image.getImageURL().equals(imageURL)) {
-                    error = "Image already exists for the ticket ";
-                    return error;
+                    error.append("Image already exists for the ticket ");
+                    String result = error.toString();
+                    return result;
                 }
             }
             // Create a new ticket image object with an existing ticket
@@ -43,64 +44,48 @@ public class AssetPlusFeatureSet5Controller {
             // Add the ticket image to the ticket
             ticket.addTicketImage(ticketImage);
         } catch (Exception e){
-            error = e.getMessage();
-            return error;
+            error.append(e.getMessage());
+            String result = error.toString();
+            return result;
         }
         
-        return error;
+        String result = error.toString();
+        return result;
     }
 
-
     public static void deleteImageFromMaintenanceTicket(String imageURL, int ticketID) {
-
 
         var error = "";
         if (imageURL == null || imageURL == "") {
             error = "Image URL cannot be empty ";
-            throw new IllegalArgumentException(error);
-            
+        }else if (!imageURL.startsWith("http://") && !imageURL.startsWith("https://")) {
+            error = "Image URL must start with http:// or https://";  
         }
         if (ticketID < 0) {
-            error = "Ticket does not exist";
-            throw new IllegalArgumentException(error);
-            
-        }
-        if (!imageURL.startsWith("http://") && !imageURL.startsWith("https://")) {
-            error = "Image URL must start with http:// or https://";
-            throw new IllegalArgumentException(error);
-            
+            error = "Ticket does not exist";     
         }
         if( !MaintenanceTicket.hasWithId(ticketID) ){
             error ="Ticket does not exist ";
-            throw new IllegalArgumentException(error);
         }
 
-        try {
-            MaintenanceTicket ticket = MaintenanceTicket.getWithId(ticketID);
-
-            // Searches for the correct image to be removed from the ticket. BY cheking the
-            // urls of all iamges in the ticket
-            boolean imageFound = false;
-            for (TicketImage image : ticket.getTicketImages()) {
-                if (image.getImageURL().equals(imageURL)) {
-                    // remove the image from the maintenace ticket
-                    ticket.removeTicketImage(image);
-                    // Deletes the image
-                    image.delete();
-                    // Should the program exit ?
-                    imageFound = true;
-                    break; // Should I Exit the loop since the image is found ?
+        if (error.isEmpty()){
+            try {
+                MaintenanceTicket ticket = MaintenanceTicket.getWithId(ticketID);
+                // Searches for the correct image to be removed from the ticket. BY cheking the
+                // urls of all iamges in the ticket
+                for (TicketImage image : ticket.getTicketImages()) {
+                    if (image.getImageURL().equals(imageURL)) {
+                        // remove the image from the maintenace ticket
+                        ticket.removeTicketImage(image);
+                        // Deletes the image
+                        image.delete();
+                        // Should the program exit ?
+                        break; // Should I Exit the loop since the image is found ?
+                    }
                 }
+            } catch (RuntimeException e) {
+                error = e.getMessage();
             }
-
-            if (!imageFound) {
-                error = "Image provided is not in the ticket";
-                throw new IllegalArgumentException(error);
-
-            }
-
-        } catch (RuntimeException e) {
-            error = e.getMessage();
         }
     }
 }
