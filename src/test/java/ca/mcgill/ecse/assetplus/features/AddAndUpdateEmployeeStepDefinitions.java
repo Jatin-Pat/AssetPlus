d057adc91af68001eed82872ca7912201c0b0088
Behrad Rezaie <r.behrad121@gmail.com>
2023-10-14 12:17:40 -0400

    All methods filled in

--- src/test/java/ca/mcgill/ecse/assetplus/features/AddAndUpdateEmployeeStepDefinitions.java
+++ src/test/java/ca/mcgill/ecse/assetplus/features/AddAndUpdateEmployeeStepDefinitions.java
@@ -5,7 +5,10 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import ca.mcgill.ecse.assetplus.model.*;
+import java.util.List;
+import java.util.Map;
 
 public class AddAndUpdateEmployeeStepDefinitions {
   @Given("the following employees exist in the system \\(p11)")
@@ -63,9 +66,8 @@ public class AddAndUpdateEmployeeStepDefinitions {
   @Then("a new employee account shall exist with {string}, {string}, {string}, and {string} \\(p11)")
   public void a_new_employee_account_shall_exist_with_and_p11(String string, String string2,
       String string3, String string4) {
-    // Write code here that turns the phrase above into concrete actions
-    
-    //Checks an employee with given email exists
+
+    //Checks if employee still exists
     assertTrue(Employee.hasWithEmail(string));
     //Checks other employee attributes
     Employee existingEmployee = (Employee) User.getWithEmail(string);
@@ -79,10 +81,11 @@ public class AddAndUpdateEmployeeStepDefinitions {
   @Then("their employee account information will be updated and is now {string}, {string}, {string}, and {string} \\(p11)")
   public void their_employee_account_information_will_be_updated_and_is_now_and_p11(String string,
       String string2, String string3, String string4) {
-    //Not sure about this one, I will name an Employee variable, assuming
-    //it is instantiated in the related @When section above
-    Employee updatedEmployee;
-    assertEquals(string, updatedEmployee.getEmail());
+    
+    assertTrue(Employee.hasWithEmail(string));
+    Employee updatedEmployee = (Employee) User.getWithEmail(string);
+
+    assertEquals(string, updatedEmployee.getEmail()); //I think this will always be true
     assertEquals(string2, updatedEmployee.getName());
     assertEquals(string3, updatedEmployee.getPassword());
     assertEquals(string4, updatedEmployee.getPhoneNumber());
@@ -92,17 +95,38 @@ public class AddAndUpdateEmployeeStepDefinitions {
   }
 
   @Then("the following employees shall exist in the system \\(p11)")
-  public void the_following_employees_shall_exist_in_the_system_p11(
-      io.cucumber.datatable.DataTable dataTable) {
-    
-    
-    // Write code here that turns the phrase above into concrete actions
-    // For automatic transformation, change DataTable to one of
-    // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-    // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-    // Double, Byte, Short, Long, BigInteger or BigDecimal.
-    //
-    // For other transformations you can register a DataTableType.
+  public void the_following_employees_shall_exist_in_the_system_p11(io.cucumber.datatable.DataTable dataTable) {
+      // Write code here that turns the phrase above into concrete actions
+      // For automatic transformation, change DataTable to one of
+      // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+      // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+      // Double, Byte, Short, Long, BigInteger or BigDecimal.
+      //
+      // For other transformations you can register a DataTableType.
+      List<Map<String, String>> employeeData = dataTable.asMaps();
+      //DATA should be converted to this format:
+      //[
+      //  { "email"="jeff@ap.com", "password"="pass1", "name"="Jeff", "phoneNumber"="(555)555-5555" },
+      //  { "email"="john@ap.com", "password"="pass2", "name"="John", "phoneNumber"="(444)444-4444" }
+      //]
+  
+
+      for (Map<String, String> data : employeeData) {
+        String email = data.get("email");
+        String name = data.get("name");
+        String password = data.get("password");
+        String phoneNumber = data.get("phoneNumber");
+
+      
+        Employee employee = (Employee) User.getWithEmail(email);
+
+        assertNotNull(employee, "Employee with email: " + email + " not found");
+        assertEquals(name, employee.getName());
+        assertEquals(password, employee.getPassword());
+        assertEquals(phoneNumber, employee.getPhoneNumber());
+      
+      
     throw new io.cucumber.java.PendingException();
   }
+}
 }
