53c5bf0c57038c2e217fd039364abaf8ecf478d3
LaurencePerreault <143827146+LaurencePerreault@users.noreply.github.com>
2023-10-17 19:05:03 -0400

    Update AddAndUpdateEmployeeStepDefinitions.java

--- src/test/java/ca/mcgill/ecse/assetplus/features/AddAndUpdateEmployeeStepDefinitions.java
+++ src/test/java/ca/mcgill/ecse/assetplus/features/AddAndUpdateEmployeeStepDefinitions.java
@@ -94,7 +94,7 @@ public class AddAndUpdateEmployeeStepDefinitions {
   @Then("the number of employees in the system shall be {string} \\(p11)")
   public void the_number_of_employees_in_the_system_shall_be_p11(String string) {
     // Write code here that turns the phrase above into concrete actions
-    AssetPlus assetPlus = AssetPlusApplication.getAssetPlus(); 
+    
     // I don't know if this is right
     List<Employee> employees = assetPlus.getEmployees();
 
