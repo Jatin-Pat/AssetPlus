35d5ccdd019832c35a190905cb627ec363c7b21c
Behrad Rezaie <r.behrad121@gmail.com>
2023-11-06 15:29:02 -0500

    Created controller methods file

---
+++ src/main/java/ca/mcgill/ecse/assetplus/controller/TicketMaintenanceController.java
@@ -0,0 +1,22 @@
+package ca.mcgill.ecse.assetplus.controller;
+
+public class TicketMaintenanceController {
+  
+
+  public void assignStaffToTicket(){
+    //to implement
+  }
+  public void beginTicketWork(){
+    //to implement
+  }
+  public void completeTicketWork(){
+    //to implement
+  }
+  public void approveTicketWork(){
+    //to implement
+  }
+  public void disapproveTicketWork(){
+    //to implement
+  }
+
+}
