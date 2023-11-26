164547645a94fb301ebadaddfc8dd87efdcd2dcb
Jatin Patel <jatin_pat@outlook.com>
2023-11-26 07:06:45 -0500

    created manageAsset controller skeleton

---
+++ src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/ManageAsset.java
@@ -0,0 +1,115 @@
+package ca.mcgill.ecse.assetplus.javafx.fxml.controllers;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SplitMenuButton;
+import javafx.scene.control.TextField;
+
+public class ManageAsset {
+
+    @FXML
+    private Label AssetNumberLabel;
+
+    @FXML
+    private TextField AssetNumberTextField;
+
+    @FXML
+    private TextField AssetTypeTextField;
+
+    @FXML
+    private Label ExpectedLifespanLabel;
+
+    @FXML
+    private TextField ExpectedLifespanTextField;
+
+    @FXML
+    private Label NewAssetTypeLabel;
+
+    @FXML
+    private Label NewExpectedLifespanLabel;
+
+    @FXML
+    private MenuItem addAssetClicked;
+
+    @FXML
+    private MenuItem addAssetTypeClicked;
+
+    @FXML
+    private SplitMenuButton addMenuButton;
+
+    @FXML
+    private Label assetTypeLabel;
+
+    @FXML
+    private MenuItem deleteAssetClicked;
+
+    @FXML
+    private MenuItem deleteAssetTypeClicked;
+
+    @FXML
+    private SplitMenuButton deleteMenuButton;
+
+    @FXML
+    private Label floorNumberLabel;
+
+    @FXML
+    private TextField floorNumberTextField;
+
+    @FXML
+    private MenuItem modifyAssetClicked;
+
+    @FXML
+    private MenuItem modifyAssetTypeClicked;
+
+    @FXML
+    private SplitMenuButton modifyMenuButton;
+
+    @FXML
+    private TextField newAssetTypeTextField;
+
+    @FXML
+    private TextField newExpectedLifespanTextField;
+
+    @FXML
+    private DatePicker purchaseDateDatePicker;
+
+    @FXML
+    private Label purchaseDateLabel;
+
+    @FXML
+    private Label roomNumberLabel;
+
+    @FXML
+    private TextField roomNumberTextField;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+}
