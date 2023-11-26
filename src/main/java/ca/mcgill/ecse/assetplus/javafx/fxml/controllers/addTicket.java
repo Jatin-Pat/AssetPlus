dddc809bcd4dcec00ce25afd84f745261cc03a94
peiya <pei.geng@mail.mcgill.ca>
2023-11-26 09:24:31 -0500

    updated addTicket controller and AddTicket.fxml to reflect those changes

--- src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/addTicket.java
+++ src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/addTicket.java
@@ -4,18 +4,22 @@ import ca.mcgill.ecse.assetplus.controller.*;
 import ca.mcgill.ecse.assetplus.javafx.fxml.AssetPlusFxmlView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.sql.Date;
 import java.time.LocalDate;
 
 public class addTicket {
-    @FXML private Label assetPlusLabel;
+    @FXML private TextField ticketIdTextField;
 
-    @FXML private Label maintenanceTicketLabel;
-
-    @FXML private Label usernameLabel;
+    @FXML private TextField usernameTextField;
 
     @FXML private Button logoutButton;
 
@@ -32,11 +36,31 @@ public class addTicket {
     @FXML private Button submitButton;
 
     @FXML public void addTicket(ActionEvent event) {
-        int generatedID = 0; //get next integer in list of tickets
+        int generatedID = Integer.parseInt(ticketIdTextField.getText()); //get next integer in list of tickets
         Date todayDate = Date.valueOf(LocalDate.now());
+
         String description = descriptionTextField.getText();
+        if (description == null) ViewUtils.showError("Description field must not be empty\n"); //unsure about the newline
+
         int assetId = Integer.parseInt(assetIdTextField.getText());
-        String userEmail = ""; //get current user's email
+        if (assetId < 0) ViewUtils.showError("Please enter a valid asset ID\n");
+
+        String userEmail = usernameTextField.getText(); //get current user's email
+        if (assetId == 0) assetId = -1; //set to -1 if there is no specified assetId
         AssetPlusFeatureSet4Controller.addMaintenanceTicket(generatedID, todayDate, description, userEmail, assetId);
+    }
+    public void openAddImagePage(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("AddImagePage.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Add Image Page");
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Handle the exception as needed
+        }
     }
 }
