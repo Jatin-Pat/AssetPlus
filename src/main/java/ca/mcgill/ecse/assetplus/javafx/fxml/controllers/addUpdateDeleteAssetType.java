08139aa0ac046a54821070a62650915a84b22c10
Jatin Patel <jatin_pat@outlook.com>
2023-11-26 12:27:42 -0500

    addUpdateDeleteAssetType controller

---
+++ src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/addUpdateDeleteAssetType.java
@@ -0,0 +1,68 @@
+package ca.mcgill.ecse.assetplus.javafx.fxml.controllers;
+
+import ca.mcgill.ecse.assetplus.controller.*;
+import java.sql.Date;
+import javafx.event.ActionEvent;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class addUpdateDeleteAssetType {
+
+    @FXML
+    private Button addAssetType;
+
+    @FXML
+    private TextField assetTypeNameTextField;
+
+    @FXML
+    private Button deleteAssetType;
+
+    @FXML
+    private TextField expectedLifespanTextField;
+
+    @FXML
+    private Button logout;
+
+    @FXML
+    private TextField newAssetTypeNameTextField;
+
+    @FXML
+    private TextField newExpectedLifespanTextField;
+
+    @FXML
+    private Button updateAssetType;
+
+    @FXML
+    void addAssetTypeClicked(ActionEvent event) {
+      String assetTypeName = newAssetTypeNameTextField.getText();
+      int expectedLifespan = Integer.parseInt(newExpectedLifespanTextField.getText());
+
+      if (ViewUtils.successful(AssetPlusFeatureSet2Controller.addAssetType(assetTypeName, expectedLifespan))){
+        newAssetTypeNameTextField.setText("");
+        newExpectedLifespanTextField.setText("");
+      }
+    }
+
+    @FXML
+    void deleteAssetTypeClicked(ActionEvent event) {
+      String assetTypeName = assetTypeNameTextField.getText();
+
+      AssetPlusFeatureSet2Controller.deleteAssetType(assetTypeName);
+      assetTypeNameTextField.setText("");
+    }
+
+    @FXML
+    void updateAssetTypeClicked(ActionEvent event) {
+      String assetTypeName = assetTypeNameTextField.getText();
+      int newExpectedLifespan = Integer.parseInt(newExpectedLifespanTextField.getText());
+      String newAssetType = newAssetTypeNameTextField.getText();
+
+      if (ViewUtils.successful(AssetPlusFeatureSet2Controller.updateAssetType(assetTypeName, newAssetType, newExpectedLifespan))){
+        assetTypeNameTextField.setText("");
+        newAssetTypeNameTextField.setText("");
+      }
+    }
+
+}
