822e380b0cfcc4fb0d0fc0de27af82f4ce6d1f8e
Jatin Patel <jatin_pat@outlook.com>
2023-11-26 07:38:03 -0500

    updated ManageAsset Controller

--- src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/ManageAsset.java
+++ src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/ManageAsset.java
@@ -1,4 +1,8 @@
 package ca.mcgill.ecse.assetplus.javafx.fxml.controllers;
+
+import ca.mcgill.ecse.assetplus.controller.*;
+import java.sql.Date;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
@@ -83,6 +87,76 @@ public class ManageAsset {
     @FXML
     private TextField roomNumberTextField;
 
+    @FXML
+    void addAssetClicked(ActionEvent event) {
+      int assetNumber = Integer.parseInt(AssetNumberTextField.getText());
+      int floorNumber = Integer.parseInt(floorNumberTextField.getText());
+      int roomNumber = Integer.parseInt(roomNumberTextField.getText());
+      Date purchaseDate = Date.valueOf(purchaseDateDatePicker.getValue());
+      String assetTypeName = AssetTypeTextField.getText();
+
+      if (ViewUtils.successful(AssetPlusFeatureSet3Controller.addSpecificAsset(assetNumber, floorNumber, roomNumber, purchaseDate, assetTypeName))){
+        AssetNumberTextField.setText("");
+        floorNumberTextField.setText("");
+        roomNumberTextField.setText("");
+        AssetTypeTextField.setText("");
+      }
+    }
+
+    @FXML
+    void addAssetTypeClicked(ActionEvent event) {
+      String assetTypeName = newAssetTypeTextField.getText();
+      int expectedLifespan = Integer.parseInt(newExpectedLifespanTextField.getText());
+
+      if (ViewUtils.successful(AssetPlusFeatureSet2Controller.addAssetType(assetTypeName, expectedLifespan))){
+        newAssetTypeTextField.setText("");
+        newExpectedLifespanTextField.setText("");
+      }
+    }
+
+    @FXML
+    void deleteAssetClicked(ActionEvent event) {
+      int assetNumber = Integer.parseInt(AssetNumberTextField.getText());
+
+      AssetPlusFeatureSet3Controller.deleteSpecificAsset(assetNumber);
+      AssetNumberTextField.setText("");
+    }
+
+    @FXML
+    void deleteAssetTypeClicked(ActionEvent event) {
+      String assetTypeName = AssetTypeTextField.getText();
+
+      AssetPlusFeatureSet2Controller.deleteAssetType(assetTypeName);
+      AssetTypeTextField.setText("");
+    }
+
+    @FXML
+    void modifyAssetClicked(ActionEvent event) {
+      int assetNumber = Integer.parseInt(AssetNumberTextField.getText());
+      int floorNumber = Integer.parseInt(floorNumberTextField.getText());
+      int roomNumber = Integer.parseInt(roomNumberTextField.getText());
+      Date purchaseDate = Date.valueOf(purchaseDateDatePicker.getValue());
+      String assetTypeName = AssetTypeTextField.getText();
+
+      if (ViewUtils.successful(AssetPlusFeatureSet3Controller.updateSpecificAsset(assetNumber, floorNumber, roomNumber, purchaseDate, assetTypeName))){
+        AssetNumberTextField.setText("");
+        floorNumberTextField.setText("");
+        roomNumberTextField.setText("");
+        AssetTypeTextField.setText("");
+      }
+    }
+
+    @FXML
+    void modifyAssetTypeClicked(ActionEvent event) {
+      String assetTypeName = AssetTypeTextField.getText();
+      int newExpectedLifespan = Integer.parseInt(newExpectedLifespanTextField.getText());
+      String newAssetType = newAssetTypeTextField.getText();
+
+      if (ViewUtils.successful(AssetPlusFeatureSet2Controller.updateAssetType(assetTypeName, newAssetType, newExpectedLifespan))){
+        AssetTypeTextField.setText("");
+        ExpectedLifespanTextField.setText("");
+      }
+    }
 
 
 
@@ -111,5 +185,7 @@ public class ManageAsset {
 
 
 
-    
+
+
+
 }
