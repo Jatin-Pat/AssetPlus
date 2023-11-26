94cf5458fd90b466f55592fe111c5ebbcd66437d
LaurencePerreault <143827146+LaurencePerreault@users.noreply.github.com>
2023-11-26 08:58:07 -0500

    Update AddNote.java

--- src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/AddNote.java
+++ src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/AddNote.java
@@ -8,7 +8,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
-public class AddImage {
+public class AddNote {
     @FXML private TextField description;
     @FXML private TextField ticketID;
     @FXML private Textfield date;
