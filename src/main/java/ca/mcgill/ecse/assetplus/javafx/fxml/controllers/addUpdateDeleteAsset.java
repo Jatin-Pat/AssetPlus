26ded3b34d75826c3d82b9a04dc5b93228fefa63
Jatin Patel <jatin_pat@outlook.com>
2023-11-27 16:25:17 -0500

    updated addDeleteModifyAsset controller and fxml

--- src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/addUpdateDeleteAsset.java
+++ src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/addUpdateDeleteAsset.java
@@ -1,5 +1,47 @@
 package ca.mcgill.ecse.assetplus.javafx.fxml.controllers;
 
+import ca.mcgill.ecse.assetplus.controller.*;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class addUpdateDeleteAsset {
-  
+
+    @FXML
+    private Button addAsset;
+
+    @FXML
+    private TextField assetNumberTextField;
+
+    @FXML
+    private TextField assetTypeTextField;
+
+    @FXML
+    private Button deleteAsset;
+
+    @FXML
+    private TextField floorNumberTextField;
+
+    @FXML
+    private TextField roomNumberTextField;
+
+    @FXML
+    private Button updateAsset;
+
+    @FXML
+    void addAssetClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deleteAssetClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updateAssetClicked(ActionEvent event) {
+
+    }
+
 }
