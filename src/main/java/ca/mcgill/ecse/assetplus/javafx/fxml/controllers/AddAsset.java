93043da1dddd87561ee66dc373b5dd888c18738a
Jatin Patel <jatin_pat@outlook.com>
2023-11-28 15:01:32 -0500

    updated add asset fxml and controler

--- src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/AddAsset.java
+++ src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/AddAsset.java
@@ -1,5 +1,69 @@
 package ca.mcgill.ecse.assetplus.javafx.fxml.controllers;
 
+import ca.mcgill.ecse.assetplus.controller.AssetPlusFeatureSet3Controller;
+import javafx.scene.control.DatePicker;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import java.io.IOException;
+
 public class AddAsset {
-  
+
+  private MainPage mainPage;
+
+    @FXML
+    private Button addAsset;
+
+    @FXML
+    private TextField assetNumberTextField;
+
+    @FXML
+    private TextField assetTypeTextField;
+
+    @FXML
+    private TextField floorNumberTextField;
+
+    @FXML
+    private Button goBack;
+
+    @FXML
+    private DatePicker purchaseDateDatePicker;
+
+    @FXML
+    private TextField roomNumberTextField;
+
+    @FXML
+    void addAsset(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goBack(ActionEvent event) {
+      try {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../MainPage.fxml"));
+        Parent root = loader.load();
+
+        // Get the current Stage
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        // Set the new root for the current Scene
+        stage.getScene().setRoot(root);
+        mainPage = loader.getController();
+
+        mainPage.selectTab(2);
+        
+    
+      } catch (IOException e) {
+        e.printStackTrace();
+        ViewUtils.showError("Error Changing Page\n");
+      }
+    }
+
 }
+
