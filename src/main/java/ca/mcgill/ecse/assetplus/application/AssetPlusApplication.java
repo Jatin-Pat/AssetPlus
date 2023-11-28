4c5e7a45ad79129c735b735140ca28b012afa89b
behradrez <r.behrad121@gmail.com>
2023-11-27 20:31:09 -0500

    assetplus manager check

--- src/main/java/ca/mcgill/ecse/assetplus/application/AssetPlusApplication.java
+++ src/main/java/ca/mcgill/ecse/assetplus/application/AssetPlusApplication.java
@@ -19,11 +19,14 @@ public class AssetPlusApplication {
 
       AssetPlus assetPlus = getAssetPlus();
 
-      
+      if(!assetPlus.hasManager()){  
+        Manager manager = new Manager("manager@ap.com", "null", "manager", "3435", assetPlus);
+        assetPlus.setManager(manager);
+
+      }
+
 
       Application.launch(AssetPlusFxmlView.class, args);
-      Manager manager = new Manager("manager@ap.com", null, "manager", null, assetPlus);
-      assetPlus.setManager(manager);
       
       // TODO Start the application user interface here
   }
