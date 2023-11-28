e14fdb4fe262b7de025675c90804f32e4b3dd083
Dmytro <dmytro.martyniuk@mail.mcgill.ca>
2023-11-27 22:26:43 -0500

    Fixed window opening

--- src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/addUpdateDeleteTicket.java
+++ src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/addUpdateDeleteTicket.java
@@ -1,19 +1,20 @@
 package ca.mcgill.ecse.assetplus.javafx.fxml.controllers;
 
-        import ca.mcgill.ecse.assetplus.application.AssetPlusApplication;
-        import javafx.event.ActionEvent;
-        import javafx.fxml.FXML;
-        import javafx.fxml.FXMLLoader;
-        import javafx.scene.Parent;
-        import javafx.scene.Scene;
-        import javafx.scene.control.TextField;
-        import ca.mcgill.ecse.assetplus.controller.*;
-        import ca.mcgill.ecse.assetplus.model.AssetPlus;
-        import javafx.stage.Stage;
+import ca.mcgill.ecse.assetplus.application.AssetPlusApplication;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import ca.mcgill.ecse.assetplus.controller.*;
+import ca.mcgill.ecse.assetplus.model.AssetPlus;
+import javafx.stage.Stage;
 
-        import java.io.IOException;
-        import java.sql.Date;
-        import java.time.LocalDate;
+import java.io.IOException;
+import java.sql.Date;
+import java.time.LocalDate;
 
 public class addUpdateDeleteTicket {
     private static AssetPlus assetPlus = AssetPlusApplication.getAssetPlus(); // can I do this?
@@ -43,9 +44,11 @@ public class addUpdateDeleteTicket {
         String assetIdString = assetIdTextField.getText();
         if (!assetIdString.isEmpty()) {
             assetId = Integer.parseInt(assetIdString);
-            if (assetId < 0) ViewUtils.showError("Please enter a valid asset ID\n");
+            if (assetId < 0)
+                ViewUtils.showError("Please enter a valid asset ID\n");
         }
-        if (ViewUtils.successful(AssetPlusFeatureSet4Controller.addMaintenanceTicket(ticketId, raisedOn, description, email, assetId))){
+        if (ViewUtils.successful(
+                AssetPlusFeatureSet4Controller.addMaintenanceTicket(ticketId, raisedOn, description, email, assetId))) {
             assetIdTextField.setText("");
             emailTextField.setText("");
             descriptionTextField.setText("");
@@ -63,9 +66,11 @@ public class addUpdateDeleteTicket {
         String assetIdString = assetIdTextField.getText();
         if (!assetIdString.isEmpty()) {
             assetId = Integer.parseInt(assetIdString);
-            if (assetId < 0) ViewUtils.showError("Please enter a valid asset ID\n");
+            if (assetId < 0)
+                ViewUtils.showError("Please enter a valid asset ID\n");
         }
-        if (ViewUtils.successful(AssetPlusFeatureSet4Controller.updateMaintenanceTicket(ticketId, raisedOn, description, email, assetId))){
+        if (ViewUtils.successful(AssetPlusFeatureSet4Controller.updateMaintenanceTicket(ticketId, raisedOn, description,
+                email, assetId))) {
             assetIdTextField.setText("");
             emailTextField.setText("");
             descriptionTextField.setText("");
@@ -83,22 +88,18 @@ public class addUpdateDeleteTicket {
     @FXML
     void openAddImagePage(ActionEvent event) {
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("../pages/addDeleteImage.fxml")); //unable to load this page
-            loader.setController(new ca.mcgill.ecse.assetplus.javafx.fxml.controllers.addDeleteImage());
-
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../pages/addDeleteImage.fxml"));
             Parent root = loader.load();
 
-            Stage stage = new Stage();
-            stage.setResizable(true);
-            stage.setScene(new Scene(root));
-            stage.setTitle("Add/Delete Image Page");
-            stage.show();
-        }
-        catch (IOException e) {
+            // Get the current Stage
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            // Set the new root for the current Scene
+            stage.getScene().setRoot(root);
+        } catch (IOException e) {
+            System.err.println(e);
             e.printStackTrace();
             ViewUtils.showError("Error opening image upload page\n");
         }
     }
-
 }
-
