b40583c035213f8058ca8bdb720f27b8c25e67b2
LaurencePerreault <143827146+LaurencePerreault@users.noreply.github.com>
2023-11-28 23:54:05 -0500

    Update DeleteNote.java

--- src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/DeleteNote.java
+++ src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/DeleteNote.java
@@ -1,1 +1,26 @@
+package ca.mcgill.ecse.assetplus.javafx.fxml.controllers;
 
+import ca.mcgill.ecse.assetplus.controller.AssetPlusFeatureSet3Controller;
+import ca.mcgill.ecse.assetplus.controller.AssetPlusFeatureSet7Controller;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class DeleteNote {
+
+    @FXML
+    private TextField ticketIdTextField;
+
+    @FXML
+    private TextField indexTextField;
+
+    @FXML
+    void deleteNote(ActionEvent event) {
+      int id = Integer.parseInt(ticketIdTextField.getText());
+      int ind = Integer.parseInt(indexTextField.getText());
+
+      AssetPlusFeatureSet7Controller.deleteMaintenanceNote(id, ind);
+      ticketIdTextField.setText("");
+      indexTextField.setText("");
+    }
+}
